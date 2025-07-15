3da3ce9068b343fb6634b34f292e5dbd637bc398
tariqahmaad <tariq_muzamil@live.com>
2025-07-15 15:34:26 +0300

    style: Adjust CSS for body background attachment and refine media query for improved responsiveness on various screen sizes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: Personal
-* Updated: Jan 09 2024 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function () {
   "use strict";
 
